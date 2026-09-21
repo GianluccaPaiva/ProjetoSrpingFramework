5910bc947bb2b033895d29bb73b5fe5f42e44aa6
GianluccaPaiva <gianluccapaiva@gmail.com>
2026-09-21 14:10:09 -0300

    Close keys

--- src/main/java/br/com/spring/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/spring/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package br.com.spring.screenmatch;
 import br.com.spring.screenmatch.model.DadosSerie;
 import br.com.spring.screenmatch.service.ConsumoApi;
 import br.com.spring.screenmatch.service.ConverteDados;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +12,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // permite que a classe seja executada como uma aplicação Spring Boot com interface
 public class ScreenmatchApplication implements CommandLineRunner {//tarefa a ser executada pós inicialização da aplicação
 
+	@Value("${omdb.api.url}")
+	private String omdbApiUrl;
+
+	@Value("${omdb.api.key}")
+	private String omdbApiKey;
+
  //metodo run é chamado quando a aplicação é iniciada, permitindo executar código adicional após o contexto do Spring ser carregado
 	@Override
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girlsapikey=6f7764bd");
+		var json = consumoApi.obterDados(omdbApiUrl + "?t=gilmore+girls&apikey=" + omdbApiKey);
 		System.out.println(json);
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
